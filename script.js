de9accb9c955ccb25a47cefbb017b7819bfab9da
canaria <tae_ta2549@hotmail.com>
2024-01-03 10:33:38 +0700

    ลองเขียนแบบไม่ใช้บูทสแตรป

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+/*Top nav*/
+function navcollapse() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.nav-icon')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+/*polygon transition*/
 window.addEventListener('scroll', function() {
-    const infos = ['info-1', 'info-2', 'info-3', 'info-4', 'info-5', 'info-6']; // แก้ไข IDs ให้ตรงกับ ID ของ element ที่ต้องการใช้งาน
+    const infos = ['info-1', 'info-2', 'info-3', 'info-4', 'info-5', 'info-6', 'info-7', 'info-8', 'info-9', 'info-10', 'info-11', 'info-12'];
 
     infos.forEach(function(id) {
         const element = document.getElementById(id);
@@ -10,7 +29,54 @@ window.addEventListener('scroll', function() {
         if (position.top < window.innerHeight && position.bottom >= 0) {
             element.style.opacity = "1";
         } else {
-            element.style.opacity = "0"; // ให้ element ที่อยู่นอกขอบเขตของ viewport มี opacity เป็น 0
+            element.style.opacity = "0";
         }
     });
 });
+
+/*modal*/
+var modal = document.getElementById("Modal");
+var mlink = document.getElementById("modal-link");
+var span = document.getElementsByClassName("close")[0];
+
+mlink.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/*Ex-Projects*/
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let bars = document.getElementsByClassName("bar");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < bars.length; i++) {
+        bars[i].className = bars[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    bars[slideIndex-1].className += " active";
+}
