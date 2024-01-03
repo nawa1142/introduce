8bf44792779e6b1d3f73c1d811d121cdee1bd4d6
Canaria <151976828+nawa1142@users.noreply.github.com>
2024-01-03 10:41:09 +0700

    ปรับเป็น css pure

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/*Top nav*/
+function navcollapse() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.nav-icon')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+/*polygon transition*/
 window.addEventListener('scroll', function() {
     const infos = ['info-1', 'info-2', 'info-3', 'info-4', 'info-5', 'info-6', 'info-7', 'info-8', 'info-9', 'info-10', 'info-11', 'info-12'];
 
@@ -14,3 +33,50 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+
+/*modal*/
+var modal = document.getElementById("Modal");
+var mlink = document.getElementById("modal-link");
+var span = document.getElementsByClassName("close")[0];
+
+mlink.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/*Ex-Projects*/
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let bars = document.getElementsByClassName("bar");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < bars.length; i++) {
+        bars[i].className = bars[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    bars[slideIndex-1].className += " active";
+}
